e1a2731a955b12bd530641c60db23c31dabb681a
Dipanshu-mishra <mdipanshu464@gmail.com>
2024-01-27 17:13:30 +0530

    added-button

--- src/components/JokeCard.tsx
+++ src/components/JokeCard.tsx
@@ -4,6 +4,17 @@ import { CUSTOM_JOKE, API_ENDPOINT, HEADERS } from "../config";
 
 function JokeCard() {
   const [joke, setJoke] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise(resolve => setTimeout(resolve, 100));
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
 
   useEffect(() => {
     fetch(API_ENDPOINT, HEADERS)
@@ -15,16 +26,37 @@ function JokeCard() {
         console.log(err);
         setJoke(CUSTOM_JOKE);
       });
-  }, []);
+  }, [refresh]);
+
+  const handleRefresh = () => {
+    setRefresh(!refresh); 
+  };
 
   return (
+    <div className="container">
+       { loading ? (
+       <div className="spinner">
+      <div className="spinner-border text-primary justify-center" role="status">
+      <span className="sr-only"></span>
+    </div>
+    </div>
+          ) : (
+    <div>        
     <div className="card">
+     
       <div className="card-body">
         <i style={{ color: "grey", fontWeight: "bold", fontSize: "20px" }}>
           {joke}
         </i>
-      </div>
+      </div> 
     </div>
+    <div className="refreshButton">
+    <button type="button" className="btn btn-primary" onClick={handleRefresh}>
+    Refresh
+  </button>
+  </div>
+  </div>)}
+  </div>
   );
 }
 
